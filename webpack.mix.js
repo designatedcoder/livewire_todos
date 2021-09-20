29da9c77817eaa98b6e02fe4892878d80587fbb4
designatedcoder <niko@designatedcoder.com>
2021-09-19 21:28:14 -0400

    disabled notifications

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,7 +15,8 @@ mix.js('resources/js/app.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
         require('postcss-import'),
         require('tailwindcss'),
-    ]);
+    ])
+    .disableNotifications();
 
 if (mix.inProduction()) {
     mix.version();
